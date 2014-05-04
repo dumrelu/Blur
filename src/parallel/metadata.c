b6b606c19e3c5601b89f0d5725acd8dbf621264f
dumrelu <dum.relu@gmail.com>
2014-05-04 20:58:37 +0300

    Init and print metadata

---
+++ src/parallel/metadata.c
@@ -0,0 +1,19 @@
+#include "metadata.h"
+
+void meta_init(METADATA *meta, int width, int height, int radius, double sigma)
+{
+        //Set info
+        meta.width = width;
+        meta.height = height;
+        meta.radius = radius;
+        meta.sigma = sigma;
+}
+
+void meta_print(METADATA *meta)
+{
+	printf("Metadata:\n");
+	printf("\t->width=%d\n", meta->width);
+	printf("\t->height=%d\n", meta->height);
+	printf("\t->radius=%d\n", meta->radius);
+	printf("\t->=%lf\n", meta->sigma);
+}
